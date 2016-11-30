c4e17b2f7433f0caeb812a35d740fb1da6616d3f
IMustTo <cxjbinfo@gmail.com>
2016-11-30 11:59:14 +0800

    common feedback

--- src/components/common.js
+++ src/components/common.js
@@ -5,10 +5,17 @@ import CellWapper from './cell/CellWapper';
 import CellAccess from './cell/CellAccess';
 import CellBase from './cell/CellBase';
 
+import ActionSheet from './feedback/ActionSheet';
+import WeuiDialog from './feedback/WeuiDialog';
+import WeuiLoading from './feedback/WeuiLoading';
+
 export default [
   WeuiBtn,
   CellTitle,
   CellWapper,
   CellAccess,
   CellBase,
+  ActionSheet,
+  WeuiDialog,
+  WeuiLoading,
 ];
--- src/components/feedback/ActionSheet.vue
+++ src/components/feedback/ActionSheet.vue
@@ -1,12 +1,14 @@
 <template>
 <div>
   <div class="weui-mask" id="iosMask" style="opacity: 1;"></div>
-  <div class="weui-actionsheet weui-actionsheet_toggle" id="iosActionsheet">
+
+  <div class="weui-actionsheet weui-actionsheet_toggle">
     <div class="weui-actionsheet__menu">
-      <div class="weui-actionsheet__cell">示例菜单</div>
-      <div class="weui-actionsheet__cell">示例菜单</div>
-      <div class="weui-actionsheet__cell">示例菜单</div>
-      <div class="weui-actionsheet__cell">示例菜单</div>
+      <template v-for="(item, index) in sheets">
+        <div class="weui-actionsheet__cell"
+          :data-index="index"
+          @click="tapItem">{{ item }}</div>
+      </template>
     </div>
     <div class="weui-actionsheet__action">
       <div class="weui-actionsheet__cell">取消</div>
@@ -19,11 +21,19 @@
 export default {
   name: 'action-sheet',
   props: {
+    sheets: {
+      type: Array,
+    },
+  },
 
+  methods: {
+    tapItem({ target: { dataset: { index } } }) {
+      this.$emit('tapItem', index);
+    },
+
+    tapCancel() {
+      this.$emit('cancel');
+    },
   },
 };
 </script>
-
-<style>
-
-</style>
