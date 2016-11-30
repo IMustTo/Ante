f654b2710c5dcb716928ea2ee53285146fa4cd7d
IMustTo <cxjbinfo@gmail.com>
2016-11-30 23:08:30 +0800

    ui-selector

---
+++ src/components/input/UiSelectItem.vue
@@ -0,0 +1,61 @@
+<template>
+<ul id="selectclass_content">
+  <li class="ui-selector-item selector-class" :class="cls">
+    <h3 class="ui-border-b">
+      <p>{{ name }}</p>
+    </h3>
+
+    <template v-if="children.length" v-for="item in children">
+      <ui-select-item
+        :name="item.name"
+        :id="item.id"
+        :children="item.children">
+      </ui-select-item>
+    </template>
+
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: 'ui-select-item',
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+
+    id: {
+      type: Number,
+      default: 0,
+    },
+
+    children: {
+      type: Array,
+      default: [],
+    },
+  },
+
+  data() {
+    return {
+      active: false,
+    };
+  },
+
+  computed: {
+    cls() {
+      return {
+        'ante-active': this.active,
+      };
+    },
+  },
+
+  methods: {
+    tapTitle() {
+      this.active = !this.active;
+      this.$emit('tapEvt', this.id);
+    },
+  },
+};
+</script>
---
+++ src/views/SelectClass.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="page">
+
+</div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'select-class',
+
+  computed: {
+    ...mapGetters({}),
+  },
+
+  created() {
+
+  },
+
+  methods: {
+    ...mapActions([]),
+  },
+};
+</script>
+
+<style>
+
+</style>
